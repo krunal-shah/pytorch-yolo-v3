c79017334bd6fa9e8d484076a0b8a1835ca2f6b8
Krunal Shah <ktgshah@gmail.com>
2019-02-24 19:02:26 +0530

    Added debug statements

--- preprocess.py
+++ preprocess.py
@@ -40,6 +40,8 @@ def prep_image(img, inp_dim):
     print(orig_im)
     dim = orig_im.shape[1], orig_im.shape[0]
     img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
+    print("Resized image ")
+    print(img)
     img_ = img[:,:,::-1].transpose((2,0,1)).copy()
     img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
     return img_, orig_im, dim
